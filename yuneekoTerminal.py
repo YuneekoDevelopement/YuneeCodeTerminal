11a8bcec0654678f72a6537613475575ea677bc5
Yuneeko Dévelopement <121449659+YuneekoDevelopement@users.noreply.github.com>
2022-12-26 17:29:34 +0100

    YuneeCodeTerminalBeta0.0.01

---
+++ yuneekoTerminal.py
@@ -0,0 +1,38 @@
+
+def main():
+    action = input ("Séléctionner une action avec l'application Yuneeko App: ")
+    if action == "taskTerminalDebug":
+        print ("SUCCESSFULLY DEBUG")
+        main()
+    if action == "taskTerminalInfos":
+        print ("SUCCESSFULLY EXECUTED")
+        print ("Infos sur le Terminal: YuneekoTerminal Beta v1.0.0")
+        main()
+    if action == "taskOpenYNfile_$infos.yn":
+        print ("YuneekoCode 1.2.80.214 Beta")
+        main()
+    if action == "cl":
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        main()
+    if action == "select -location aep4.exe":
+        print ("C:/AppData/Roaming/AfterStudios/aep4.exe")
+
+
+
+main()
